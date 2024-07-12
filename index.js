0ac6574a54c4e99cb6292ac9c1c0a66937b69e62
willk0814 <wkoenig0814@gmail.com>
2024-07-12 15:19:34 -0400

    cors policy updated

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // Imports
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config()
-
 const routes = require("./routes")
 
 // Declare a new express app and a port to use
 const app = express();
 const port = 3000;
+
+// define allowed origins
+const allowedOrigins = [
+    'http://localhost:5173',   // local developement front end
+    'https://musicDashboard.willkoenig.info'    // production front end
+]
+
+// define cors to allow cross origin requests
+app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 // Connect to database
 mongoose
