e0f1ab6672bc54bb06365f8310a3b9c3474a34b0
willk0814 <wkoenig0814@gmail.com>
2024-07-15 17:06:38 -0400

    changed cors options

--- index.js
+++ index.js
@@ -17,17 +17,26 @@ const allowedOrigins = [
 ]
 
 // define cors to allow cross origin requests
-app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  }));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     }
+//   }));
+
+const corsOptions = {
+  origin: '*', // Replace with your Vercel app's URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+ };
+ 
+ app.use(cors(corsOptions));
 
 // Connect to database
 mongoose
